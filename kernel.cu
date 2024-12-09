e2887d445ae059b773c3c1281b2774427f1309cf
MBaranErcan <mustafabaranercan23@gmail.com>
2024-12-09 15:09:47 +0300

    vector addition

--- kernel.cu
+++ kernel.cu
@@ -5,22 +5,61 @@ These example works here are intended to implement the book "CUDA by Example" by
 #include <stdio.h>
 
 #include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 #include <Windows.h>
 
 using namespace std;
 
+#define N 10
 
+// Function prototypes
 bool isKeyPressed(unsigned timeout_ms);
 void printCudaDeviceProperties();
 
+__global__ void add(int* a, int* b, int* c);
 
-int main() {
+int main(void) {
 	
 	// Print the Cuda device properties
 	printCudaDeviceProperties();
 
+	int a[N], b[N], c[N];
+	int *dev_a, *dev_b, *dev_c;
 
+	// Allocate memory on the device.
+	cudaMalloc((void**) &dev_a, N * sizeof(int));
+	cudaMalloc((void**) &dev_b, N * sizeof(int));
+	cudaMalloc((void**) &dev_c, N * sizeof(int));
+
+
+	// Fİll the arrays a and b
+	for (int i = 0; i < N; i++) {
+		a[i] = 2-i;
+		b[i] = i * i * i;
+	}
+
+	// Copy the arrays to device
+	cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
+
+	add<<<N, 1>>>(dev_a, dev_b, dev_c);
+
+
+	// Copy the result back to host
+	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+
+
+	// Print the result
+	for (int i = 0; i < N; i++) {
+		printf("a[%d]:%d + b[%d]:%d = c[%d]:%d\n", i, a[i], i, b[i], i, c[i]);
+	}
+
+	// Free the memory
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
 
 
 	// Program end
@@ -28,6 +67,14 @@ int main() {
 	while (!isKeyPressed(0)); // Wait for 10 seconds.
 
 	return 0;
+}
+
+
+// Kernel function add two arrays
+__global__ void add(int* a, int* b, int* c) {
+	int tid = blockIdx.x;
+	if (tid < N) c[tid] = a[tid] + b[tid];
+
 }
 
 // Check if a key is pressed, with a default timeout of 0 ms. 
